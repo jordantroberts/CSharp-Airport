925637111fe73c24470adb01956c1c76e4123053
Jordan Roberts <jordanroberts@talk21.com>
2019-08-01 21:19:39 +0100

    Throws error if too stormy to take off

--- Airport/Airport.cs
+++ Airport/Airport.cs
@@ -33,8 +33,16 @@ namespace ClassNameAirport
 
         public void TakeOff(Plane plane)
         {
-            planes.Remove(plane);
-            Console.WriteLine($"{ plane.Name } has departed from {AirportName}");
+            if (_weather.Forecast() != "stormy")
+            {
+                planes.Remove(plane);
+                Console.WriteLine($"{ plane.Name } has departed from {AirportName}");
+            }
+            else
+            {
+                throw new Exception("It's too stormy to take off");
+            }
+            
         }
 
         public int GetPlaneCount()
