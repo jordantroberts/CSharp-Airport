a61aadaf3a9243d52b7dcb8d017d5d9fcc536c2c
Jordan Roberts <jordanroberts@talk21.com>
2019-08-01 13:13:45 +0100

    Set up project and added Land and Take Off

---
+++ Airport/Airport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Airport
+{
+    public class Airport
+    {
+        public List<string> planes;
+
+        public Airport()
+        {
+            planes = new List<string>();
+        }
+
+        public List<string> Land(string plane)
+        {
+            planes.Add(plane);
+            planes.ForEach(Console.WriteLine);
+            return planes;
+        }
+
+        public void TakeOff(string plane)
+        {
+            planes.Remove(plane);
+        }
+
+    }
+}
---
+++ Airport/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Airport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airport airport = new Airport();
+            airport.Land("Plane 1");
+            airport.Land("Plane 2");
+        }
+    }
+}
