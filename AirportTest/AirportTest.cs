985098d3b673e987124040313d6a74d12e7f1045
Jordan Roberts <jordanroberts@talk21.com>
2019-08-01 18:59:06 +0100

    Added test for TakeOff

--- AirportTest/AirportTest.cs
+++ AirportTest/AirportTest.cs
@@ -7,15 +7,23 @@ namespace AirportTest
     public class AirportTest
     {
 
+        // moq these later
+        Airport airport = new Airport("TestAirport");
+        Plane plane = new Plane("TestPlane");
+        // moq these later
         [Test]
+       
         public void PlaneCanLand()
         {
-            // moq these later
-            Airport airport = new Airport("TestAirport");
-            Plane plane = new Plane("TestPlane");
-            // moq these later
             airport.Land(plane);
             Assert.IsTrue(airport.planes.Contains(plane));
+        }
+
+        public void PlaneCanTakeOff()
+        {
+            airport.Land(plane);
+            airport.TakeOff(plane);
+            Assert.IsFalse(airport.planes.Contains(plane));
         }
     }
 }
